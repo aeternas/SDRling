c1026c5e4a5707cccbc695b36052d549eaf17351
Ivan Golikov <aeternass@gmail.com>
2020-04-14 22:29:24 +0300

    Added env injection

--- pre_deployment.sh
+++ pre_deployment.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sed -i '' 's/SOCKET_URL/'$TEST_ENV'/g' "./SDRling/Modules/Listening/ReceiverService.swift"
+sed -i '' 's/WS_KEY/'$WS_KEY'/g' "./SDRling/Modules/Listening/ReceiverService.swift"
