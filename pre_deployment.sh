5bab7264e05091d2edc4d6c5a84d26d8b26d61cd
Ivan Golikov <aeternass@gmail.com>
2020-04-16 23:34:49 +0300

    Removed redundant env

--- pre_deployment.sh
+++ pre_deployment.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sed -i '' 's/SOCKET_URL/'$TEST_ENV'/g' "./SDRling/Modules/Listening/ReceiverService.swift"
 sed -i '' 's/WS_KEY/'$WS_KEY'/g' "./SDRling/Modules/Listening/ReceiverService.swift"
